e5f52045c6abef1e6eecda399f6ae1f69c67fe68
Matt Kaye <mrkaye97@gmail.com>
2024-12-02 11:46:48 -0500

    code cleanup

--- 2024/aoc2024/lib/solutions/02.exs
+++ 2024/aoc2024/lib/solutions/02.exs
@@ -1,5 +1,7 @@
 defmodule Solution do
-  def read do
+  defp max_numeric_diff_allowed(), do: 3
+
+  defp read do
     "data/02.txt"
     |> File.read!()
     |> String.split(~r{\n}, trim: true)
@@ -10,26 +12,23 @@ defmodule Solution do
     end)
   end
 
-  def check_is_safe(curr, prev) do
-    if prev == nil do
-      true
-    else
-      abs(curr - prev) <= 3 and curr != prev
-    end
+  defp check_is_safe(curr, prev) do
+    if prev == nil,
+      do: true,
+      else: abs(curr - prev) <= max_numeric_diff_allowed() and curr != prev
   end
 
-  def determine_trend(current, previous) do
+  defp determine_trend(current, previous) do
     cond do
       previous == nil -> nil
-      current > previous -> "up"
-      current < previous -> "down"
+      current > previous -> :up
+      current < previous -> :down
       true -> nil
     end
   end
 
-  def part_1(input) do
-    input
-    |> Enum.map(fn line ->
+  defp check_line_validity(line, num_failures_allowed) do
+    {failures, _, _} =
       line
       |> Enum.reduce({0, nil, nil}, fn current, {num_failures, previous, trend} ->
         current_trend = determine_trend(current, previous)
@@ -39,47 +38,33 @@ defmodule Solution do
 
         is_failure = not (trend_continuing and is_safe)
 
-        {num_failures + if(is_failure, do: 1, else: 0), current, trend || current_trend}
+        if is_failure and num_failures < num_failures_allowed do
+          {num_failures + if(is_failure, do: 1, else: 0), previous, trend}
+        else
+          {num_failures + if(is_failure, do: 1, else: 0), current, trend || current_trend}
+        end
       end)
-    end)
-    |> Enum.map(fn {num_failures, _, _} -> num_failures end)
-    |> Enum.reduce(0, fn x, num_failures -> num_failures + if(x == 0, do: 1, else: 0) end)
+
+    failures
   end
 
-  def part_2_valid(line) do
-    line
-    |> Enum.reduce({0, nil, nil}, fn current, {num_failures, previous, trend} ->
-      current_trend = determine_trend(current, previous)
+  defp compute_num_valid_lines(input, num_failures_allowed) do
+    Enum.reduce(input, 0, fn line, acc ->
+      failures =
+        min(
+          check_line_validity(line, num_failures_allowed),
+          check_line_validity(Enum.reverse(line), num_failures_allowed)
+        )
 
-      is_safe = check_is_safe(current, previous)
-      trend_continuing = trend == nil or current_trend == trend
-
-      is_failure = not (trend_continuing and is_safe)
-
-      if is_failure and num_failures == 0 do
-        {1, previous, trend}
-      else
-        {num_failures + if(is_failure, do: 1, else: 0), current, trend || current_trend}
-      end
+      acc + if(failures <= num_failures_allowed, do: 1, else: 0)
     end)
-  end
-
-  def part_2(input) do
-    input
-    |> Enum.map(fn line ->
-      {ffail, _, _} = part_2_valid(line)
-      {bfail, _, _} = part_2_valid(Enum.reverse(line))
-
-      min(ffail, bfail)
-    end)
-    |> Enum.reduce(0, fn x, num_failures -> num_failures + if(x <= 1, do: 1, else: 0) end)
   end
 
   def main do
     data = read()
 
-    IO.puts(part_1(data))
-    IO.puts(part_2(data))
+    IO.puts(compute_num_valid_lines(data, 0))
+    IO.puts(compute_num_valid_lines(data, 1))
   end
 end
 
