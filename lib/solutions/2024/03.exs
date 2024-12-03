f08302ce828dae07e8068f9be53de04d3a8e3be0
Matt Kaye <mrkaye97@gmail.com>
2024-12-03 09:18:39 -0500

    day 3 code cleanup

--- lib/solutions/2024/03.exs
+++ lib/solutions/2024/03.exs
@@ -1,55 +1,54 @@
 defmodule Solution do
-  def parse_input_part_1(input) do
+  def extract_valid_mul_statements(input) do
     regex = ~r/mul\(\d{1,3},\d{1,3}\)/
 
     Regex.scan(regex, input) |> List.flatten()
   end
 
-  def parse_input_part_2(input) do
-    String.split(input, ~r/(?=(do\(\)|don't\(\)))/)
-    |> Enum.with_index()
-    |> Enum.filter(fn {x, ix} ->
-      ix == 0 or String.slice(x, 0, 4) == "do()"
-    end)
-    |> Enum.map(fn {x, _} ->
-      x
-    end)
-    |> Enum.join("")
-    |> parse_input_part_1()
-    |> Enum.map(&eval_mul_statement/1)
-    |> Enum.sum()
-    |> Integer.to_string()
-  end
-
   def eval_mul_statement(statement) do
     statement
-    |> String.replace("mul(", "")
-    |> String.replace(")", "")
+    |> String.replace(~r/(mul\(|\))/, "")
     |> String.split(",")
     |> Enum.map(&String.to_integer/1)
     |> Enum.product()
   end
 
-  def part_1(input) do
+  def aggregate_muls_in_string(input) do
     input
-    |> parse_input_part_1()
+    |> extract_valid_mul_statements()
     |> Enum.map(&eval_mul_statement/1)
     |> Enum.sum()
+  end
+
+  def extract_enabled_substrings(input) do
+    input
+    |> String.split(~r/(?=(do\(\)|don't\(\)))/)
+    |> Enum.with_index()
+    |> Enum.filter(fn {x, ix} ->
+      ix == 0 or String.slice(x, 0, 4) == "do()"
+    end)
+    |> Enum.map(fn {x, _} -> x end)
+    |> Enum.join("")
+  end
+
+  def part_1(input) do
+    input
+    |> aggregate_muls_in_string()
     |> Integer.to_string()
   end
 
   def part_2(input) do
     input
-    |> parse_input_part_2()
+    |> extract_enabled_substrings()
+    |> aggregate_muls_in_string()
+    |> Integer.to_string()
   end
 
   def main do
-    data =
-      "data/2024/03.txt"
-      |> File.read!()
+    input = File.read!("data/2024/03.txt")
 
-    IO.puts("Part I: " <> part_1(data))
-    IO.puts("Part II: " <> part_2(data))
+    IO.puts("Part I: " <> part_1(input))
+    IO.puts("Part II: " <> part_2(input))
   end
 end
 
