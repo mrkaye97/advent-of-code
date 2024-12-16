69430f5f440f232d19ce3be8d71384b7072fc7c8
Matt Kaye <mrkaye97@gmail.com>
2024-12-16 12:44:19 -0500

    day 2024, part 1

--- lib/solutions/2024/16.exs
+++ lib/solutions/2024/16.exs
@@ -6,10 +6,10 @@ defmodule Solution do
 
   defp parse_input(input) do
     input
-    |> Enum.map(&String.graphemes/1)
     |> Enum.with_index()
     |> Enum.reduce(%{}, fn {row, row_index}, acc ->
       row
+      |> String.graphemes()
       |> Enum.with_index()
       |> Enum.reduce(acc, fn {cell, col_index}, acc ->
         Map.put(acc, {row_index, col_index}, cell)
@@ -18,95 +18,106 @@ defmodule Solution do
   end
 
   defp find_square_by_value(input, value) do
-    input
-    |> Enum.find(fn {_, v} -> v == value end)
-    |> elem(0)
+    Enum.find_value(input, fn {coords, v} -> if v == value, do: coords end)
   end
 
   defp add_rotation_edges(graph, position) do
-    Enum.reduce(@directions, graph, fn direction, graph2 ->
-      Enum.reduce(@directions, graph2, fn direction2, graph3 ->
-        if direction != direction2 do
+    Enum.reduce(@directions, graph, fn direction, acc_graph ->
+      Enum.reduce(@directions -- [direction], acc_graph, fn other_direction, g ->
+        Graph.add_edge(
+          g,
+          Graph.Edge.new({position, direction}, {position, other_direction}, weight: 1000)
+        )
+      end)
+    end)
+  end
+
+  defp propose_coords({x, y}, "N"), do: {x - 1, y}
+  defp propose_coords({x, y}, "E"), do: {x, y + 1}
+  defp propose_coords({x, y}, "S"), do: {x + 1, y}
+  defp propose_coords({x, y}, "W"), do: {x, y - 1}
+
+  defp add_travel_edges(graph, coords_map) do
+    Enum.reduce(coords_map, graph, fn {coords, _}, acc_graph ->
+      Enum.reduce(@directions, acc_graph, fn direction, g ->
+        proposed = propose_coords(coords, direction)
+
+        if Map.get(coords_map, proposed) not in ["#", nil] do
           Graph.add_edge(
-            graph3,
-            Graph.Edge.new({position, direction}, {position, direction2}, weight: 1000)
+            g,
+            Graph.Edge.new({coords, direction}, {proposed, direction}, weight: 1)
           )
         else
-          graph3
+          g
         end
       end)
     end)
   end
 
-  defp propose_coords(coords, direction) do
-    cond do
-      direction == "N" -> {elem(coords, 0) - 1, elem(coords, 1)}
-      direction == "E" -> {elem(coords, 0), elem(coords, 1) + 1}
-      direction == "S" -> {elem(coords, 0) + 1, elem(coords, 1)}
-      direction == "W" -> {elem(coords, 0), elem(coords, 1) - 1}
-    end
-  end
-
-  defp add_travel_edges(graph) do
-    Enum.reduce(
-      Graph.vertices(graph),
-      graph,
-      fn {coords, direction}, graph2 ->
-        Enum.reduce(Graph.vertices(graph), graph2, fn {coords2, _}, graph3 ->
-          proposed = propose_coords(coords, direction)
-
-          if coords2 == proposed do
-            Graph.add_edge(
-              graph3,
-              Graph.Edge.new({coords, direction}, {coords2, direction}, weight: 1)
-            )
-          else
-            graph3
-          end
-        end)
+  defp build_graph(data) do
+    Enum.reduce(data, Graph.new(), fn {coords, value}, graph ->
+      if value == "#" do
+        graph
+      else
+        graph
+        |> add_rotation_edges(coords)
       end
-    )
+    end)
+    |> add_travel_edges(data)
   end
 
   defp part_1(data) do
-    start_square = {find_square_by_value(data, "S"), "E"}
-    end_square = find_square_by_value(data, "E")
+    start_coords = find_square_by_value(data, "S")
+    end_coords = find_square_by_value(data, "E")
+    start_square = {start_coords, "E"}
 
-    g =
-      data
-      |> Enum.reduce(Graph.new(), fn {_, _}, graph ->
-        data
-        |> Enum.reduce(graph, fn {coords2, value2}, graph2 ->
-          IO.inspect(coords2)
-
-          if value2 == "#" do
-            graph2
-          else
-            add_rotation_edges(graph2, coords2)
-          end
-        end)
-      end)
-      |> add_travel_edges()
+    graph = build_graph(data)
 
     @directions
     |> Enum.map(fn direction ->
-      Graph.dijkstra(g, start_square, {end_square, direction})
-      |> Enum.chunk_every(2, 1, :discard)
-      |> Enum.map(fn [from, to] -> Graph.edge(g, from, to).weight end)
-      |> Enum.sum()
+      case Graph.dijkstra(graph, start_square, {end_coords, direction}) do
+        nil ->
+          :infinity
+
+        path ->
+          Enum.chunk_every(path, 2, 1, :discard)
+          |> Enum.reduce(0, fn [from, to], acc ->
+            acc + Graph.edge(graph, from, to).weight
+          end)
+      end
     end)
     |> Enum.min()
   end
 
-  defp part_2(input) do
-    input
+  defp part_2(data) do
+    start_coords = find_square_by_value(data, "S")
+    end_coords = find_square_by_value(data, "E")
+    start_square = {start_coords, "E"}
+
+    graph = build_graph(data)
+
+    @directions
+    |> Enum.map(fn direction ->
+      paths = Graph.Pathfindings.BellmanFord(graph, start_square, {end_coords, direction})
+
+      paths
+      |> Enum.map(fn path ->
+        path
+        |> Enum.chunk_every(2, 1, :discard)
+        |> Enum.reduce(0, fn [from, to], acc ->
+          acc + Graph.edge(graph, from, to).weight
+        end)
+      end)
+      |> IO.inspect()
+    end)
+    |> Enum.min()
   end
 
   def main do
     data = read_input(2024, 16) |> parse_input()
 
     run_solution(1, &part_1/1, data)
-    # run_solution(2, &part_2/1, data)
+    run_solution(2, &part_2/1, data)
   end
 end
 
