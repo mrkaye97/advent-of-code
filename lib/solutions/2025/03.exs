c25129ace3789a8435b0a25c842d625e915fcf85
mrkaye97 <mrkaye97@gmail.com>
2025-12-03 17:27:08 -0500

    feat: pt ii

--- lib/solutions/2025/03.exs
+++ lib/solutions/2025/03.exs
@@ -13,61 +13,32 @@ defmodule Solution do
     Enum.find_index(xs, fn x -> x == Enum.max(xs) end)
   end
 
-  defp split_at_pivot(xs, pivot) do
-    {left, [_max | right]} = Enum.split(xs, pivot)
+  defp compute_joltage(input, n) do
+    Enum.sum_by(input, fn bank ->
+      Enum.reduce(n..1//-1, ["", bank], fn ix, [acc, b] ->
+        candidates = Enum.slice(b, 0..(length(b) - ix))
+        v = Enum.max(candidates)
+        i = argmax(candidates)
 
-    [left, right]
-  end
+        acc = acc <> Integer.to_string(v)
 
-  defp compute_joltage(bank, n) do
-    pivot = argmax(bank)
-    value = Enum.at(bank, pivot)
-
-    bank
-    |> split_at_pivot(pivot)
-    |> Enum.map(fn x ->
-      cond do
-        Enum.empty?(x) -> nil
-        true -> Enum.max(x)
-      end
+        [acc, Enum.slice(b, (i + 1)..length(b))]
+      end)
+      |> Enum.at(0)
+      |> String.to_integer()
     end)
-    |> Enum.with_index()
-    |> Enum.map(fn {x, i} ->
-      cond do
-        is_nil(x) -> nil
-        i == 0 -> String.to_integer(Integer.to_string(x) <> Integer.to_string(value))
-        i == 1 -> String.to_integer(Integer.to_string(value) <> Integer.to_string(x))
-      end
-    end)
-    |> Enum.reject(&is_nil/1)
-    |> Enum.max()
   end
 
   defp part_1(input) do
-    Enum.sum_by(input, fn x -> compute_joltage(x, 2) end)
+    compute_joltage(input, 2)
   end
 
   defp part_2(input) do
-    # find the leftmost biggest number with at least n-1 values to the right of it
-    # find the leftmost biggest number after that with at least n-2 values to the right of it
-    # ...
-    input
-    |> Enum.map(fn bank ->
-      Enum.reduce
-      candidates = Enum.slice(bank, 1..(length(bank) - 12))
-      max_value = Enum.max(bank)
-      max_index = argmax(bank)
-
-      IO.inspect({candidates, max_value, max_index})
-    end)
-
-    1
+    compute_joltage(input, 12)
   end
 
   def main do
     data = read_input(2025, 03) |> parse_input()
-
-    IO.inspect(data)
 
     run_solution(1, &part_1/1, data)
     run_solution(2, &part_2/1, data)
