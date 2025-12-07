6a3bf3a3eabee8ef44cf9565a67e8dd40cce5e87
mrkaye97 <mrkaye97@gmail.com>
2025-12-07 12:10:56 -0500

    feat: 7, 2, attempt 1

--- lib/solutions/2025/07.exs
+++ lib/solutions/2025/07.exs
@@ -49,17 +49,63 @@ defmodule Solution do
     end)
   end
 
-  defp part_2(input) do
-    input
+  defp part_2(grid) do
+    {_, height} = Grid.dim(grid)
+    tree = Graph.new()
+
+    Enum.reduce_while(0..height, {grid, [Grid.find(grid, "S")], 0, tree}, fn _,
+                                                                             {g, row, count, t} ->
+      t = Enum.reduce(row, t, fn coords, acc -> Graph.add_vertex(acc, coords) end)
+
+      candidates =
+        Enum.map(row, fn coords ->
+          pos = Grid.find_neighbor_pos(coords, Grid.south())
+
+          {pos, Grid.get(g, pos)}
+        end)
+
+      cond do
+        not Enum.any?(Enum.map(candidates, fn {_, v} -> v end)) ->
+          {:halt,
+           {count,
+            Enum.map(candidates, fn {pos, _} -> Grid.find_neighbor_pos(pos, Grid.north()) end)},
+           t}
+
+        true ->
+          {g, next, c, t} =
+            candidates
+            |> Enum.reduce({g, MapSet.new(), count, t}, fn {coords, val}, {acc, next, c, t} ->
+              cond do
+                val == "^" ->
+                  east_neighbor = Grid.find_neighbor_pos(coords, Grid.east())
+                  west_neighbor = Grid.find_neighbor_pos(coords, Grid.west())
+
+                  {acc
+                   |> Grid.set(east_neighbor, "|")
+                   |> Grid.set(west_neighbor, "|"),
+                   next |> MapSet.put(east_neighbor) |> MapSet.put(west_neighbor), c + 1,
+                   t
+                   |> Graph.add_edge(Grid.find_neighbor_pos(coords, Grid.north()), east_neighbor)
+                   |> Graph.add_edge(Grid.find_neighbor_pos(coords, Grid.north()), west_neighbor)}
+
+                true ->
+                  {acc |> Grid.set(coords, "|"), MapSet.put(next, coords), c,
+                   t |> Graph.add_edge(Grid.find_neighbor_pos(coords, Grid.north()), coords)}
+              end
+            end)
+
+          {:cont, {g, next, c, t}}
+      end
+    end)
+    |> IO.inspect()
+    |> elem(0)
   end
 
   def main do
     data = read_input(2025, 07) |> parse_input()
 
-    IO.inspect(data)
-
     run_solution(1, &part_1/1, data)
-    # run_solution(2, &part_2/1, data)
+    run_solution(2, &part_2/1, data)
   end
 end
 
