0517bc713579e917136e4b2f09d35ccef00e8910
mrkaye97 <mrkaye97@gmail.com>
2025-12-16 19:58:53 -0500

    feat: part 2, much faster

--- lib/solutions/2025/11.exs
+++ lib/solutions/2025/11.exs
@@ -22,42 +22,60 @@ defmodule Solution do
     |> length()
   end
 
-  defmemo dfs(_graph, curr, visited_dac, visited_fft) when curr == "out" do
-    if visited_dac and visited_fft, do: 1, else: 0
-  end
+  defp dfs(graph, curr, visited_dac, visited_fft, cache) do
+    key = {curr, visited_dac, visited_fft}
 
-  defmemo dfs(graph, curr, visited_dac, visited_fft) do
-    graph
-    |> Map.get(curr, [])
-    |> Enum.reduce(0, fn child, total ->
-      total +
-        dfs(
-          graph,
-          child,
-          visited_dac or child == "dac",
-          visited_fft or child == "fft"
-        )
-    end)
+    case Agent.get(cache, &Map.get(&1, key)) do
+      nil ->
+        result =
+          if curr == "out" do
+            if visited_dac and visited_fft, do: 1, else: 0
+          else
+            graph
+            |> Map.get(curr, [])
+            |> Enum.reduce(0, fn child, total ->
+              total +
+                dfs(
+                  graph,
+                  child,
+                  visited_dac or child == "dac",
+                  visited_fft or child == "fft",
+                  cache
+                )
+            end)
+          end
+
+        Agent.update(cache, &Map.put(&1, key, result))
+        result
+
+      cached ->
+        cached
+    end
   end
 
   defp part_2(input) do
-    input
-    |> Enum.reduce(Graph.new(), fn {k, vs}, g ->
-      Enum.reduce(vs, g, fn v, g ->
-        g
-        |> Graph.add_vertex(v)
-        |> Graph.add_edge(k, v)
+    graph =
+      input
+      |> Enum.reduce(Graph.new(), fn {k, vs}, g ->
+        Enum.reduce(vs, g, fn v, g ->
+          g
+          |> Graph.add_vertex(v)
+          |> Graph.add_edge(k, v)
+        end)
       end)
-    end)
-    |> Graph.edges()
-    |> Enum.reduce(Map.new(), fn e, acc ->
-      from = e.v1
-      to = e.v2
-      existing = Map.get(acc, from, [])
+      |> Graph.edges()
+      |> Enum.reduce(Map.new(), fn e, acc ->
+        from = e.v1
+        to = e.v2
+        existing = Map.get(acc, from, [])
 
-      Map.put(acc, from, [to | existing])
-    end)
-    |> dfs("svr", false, false)
+        Map.put(acc, from, [to | existing])
+      end)
+
+    {:ok, cache} = Agent.start_link(fn -> %{} end)
+    result = dfs(graph, "svr", false, false, cache)
+    Agent.stop(cache)
+    result
   end
 
   def main do
