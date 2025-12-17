25504b021dc2146ac4b1f8ac7f1f82226c31bcd1
mrkaye97 <mrkaye97@gmail.com>
2025-12-16 19:51:31 -0500

    feat: 2025, 11, part 2 slow impl

--- lib/solutions/2025/11.exs
+++ lib/solutions/2025/11.exs
@@ -1,6 +1,7 @@
 defmodule Solution do
   import Common.Input
   import Common.Output
+  use Memoize
 
   defp parse_input(input) do
     input
@@ -9,21 +10,62 @@ defmodule Solution do
   end
 
   defp part_1(input) do
-    graph =
-      input
-      |> Enum.reduce(Graph.new(), fn {k, vs}, g ->
-        Enum.reduce(vs, g, fn v, g ->
-          g
-          |> Graph.add_vertex(v)
-          |> Graph.add_edge(k, v)
-        end)
+    input
+    |> Enum.reduce(Graph.new(), fn {k, vs}, g ->
+      Enum.reduce(vs, g, fn v, g ->
+        g
+        |> Graph.add_vertex(v)
+        |> Graph.add_edge(k, v)
       end)
-      |> Graph.Pathfinding.all("you", "out")
-      |> length()
+    end)
+    |> Graph.Pathfinding.all("you", "out")
+    |> length()
+  end
+
+  defmemo dfs(_graph, curr, target, stack, visited_dac, visited_fft) when curr == target do
+    cond do
+      visited_dac and visited_fft -> 1
+      true -> 0
+    end
+  end
+
+  defmemo dfs(graph, curr, target, stack, visited_dac, visited_fft) do
+    graph
+    |> Map.get(curr, [])
+    |> Enum.filter(fn child ->
+      !Enum.member?(stack, child)
+    end)
+    |> Enum.reduce(0, fn child, total ->
+      total +
+        dfs(
+          graph,
+          child,
+          target,
+          [child | stack],
+          visited_dac or child == "dac",
+          visited_fft or child == "fft"
+        )
+    end)
   end
 
   defp part_2(input) do
     input
+    |> Enum.reduce(Graph.new(), fn {k, vs}, g ->
+      Enum.reduce(vs, g, fn v, g ->
+        g
+        |> Graph.add_vertex(v)
+        |> Graph.add_edge(k, v)
+      end)
+    end)
+    |> Graph.edges()
+    |> Enum.reduce(Map.new(), fn e, acc ->
+      from = e.v1
+      to = e.v2
+      existing = Map.get(acc, from, [])
+
+      Map.put(acc, from, [to | existing])
+    end)
+    |> dfs("svr", "out", [], false, false)
   end
 
   def main do
@@ -32,7 +74,7 @@ defmodule Solution do
     IO.inspect(data)
 
     run_solution(1, &part_1/1, data)
-    # run_solution(2, &part_2/1, data)
+    run_solution(2, &part_2/1, data)
   end
 end
 
